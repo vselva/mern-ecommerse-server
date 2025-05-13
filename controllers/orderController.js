413abab325826556e63d3f7e0e67b2f13c70fd60
Selvakumar <vselva@gmail.com>
2025-05-13 19:22:10 +0530

    Projection in Order Details and Orders APIs

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,29 @@
-
+const mongoose = require('mongoose');
 const Order = require('../models/Order');
 const Product = require('../models/Product');
 
 const getOrders = async (req, res) => {
     try {
-        const orders = await Order.find().limit(10);
+        const orders = await Order.aggregate([
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "userInfo"
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    orderDate: "$createdAt",
+                    totalAmount: 1,
+                    user: 1,
+                    "userInfo._id": 1,
+                    "userInfo.email": 1
+                }
+            }
+        ]);
         res.status(200).json(orders);
     } catch (err) {
         console.log('Error in getting order list. Error: ', err);
@@ -15,10 +34,37 @@ const getOrders = async (req, res) => {
 const getOrderById = async (req, res) => {
     try {
         const orderId = req.params.id;
-        const orderDetails = await Order.findById({_id: orderId});
+        
+        // const orderDetails = await Order.findById({_id: orderId});
+        const orderDetails = await Order.aggregate([
+            {
+                $match: {
+                    _id: mongoose.Types.ObjectId.createFromHexString(orderId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "products",
+                    localField: "products.product",
+                    foreignField: "_id",
+                    as: "productDetails"
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    totalAmount: 1,
+                    user: 1, 
+                    orderDate: "$createdAt",
+                    productDetails: 1
+                }
+            }
+        ]);
+
         if (!orderDetails) {
             return res.status(401).json({ error: 'Order not found' });
         }
+
         res.status(200).json(orderDetails);
     } catch (err) {
         console.log('Error in Fetching Order Details: Error:', err);
