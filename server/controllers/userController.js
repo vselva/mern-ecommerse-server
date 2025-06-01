d3a5857c87693a0eb78540ca0a36ba314bb5930b
Selvakumar <vselva@gmail.com>
2025-06-01 13:52:41 +0530

    Moved code to separate folder server, to keep react code in client folder

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,7 +18,7 @@ const register = async (req, res) => {
         const savedUser = await newUser.save();
         res.status(201).json({ message: 'user registered', id: savedUser._id });
     } catch (err) {
-        console.log('Error in registering an UserError: ' + err);
+        console.log('Error in registering an User. Error: ' + err);
         res.status(400).json({error: 'Error in registering an user.'});
     }
 }
