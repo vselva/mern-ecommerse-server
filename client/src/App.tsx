dd7fb2f969ca0e4158cb305f61baa823c88e4c11
Selvakumar <vselva@gmail.com>
2025-06-01 15:59:58 +0530

    Added React as Client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return <>
+    <div className="container mt-5">
+      <div className="row">
+        <h1 className="text-center">Test</h1>
+      </div>
+    </div>
+  </>
+}
+
+export default App
