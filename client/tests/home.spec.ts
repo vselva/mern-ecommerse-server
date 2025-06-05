cb8b4f51908cb6efac4342bda73def332e3982bc
Selvakumar <vselva@gmail.com>
2025-06-05 17:40:49 +0530

    beforeEach and afterEach

--- client/tests/home.spec.ts
+++ client/tests/home.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from '@playwright/test';
-import { describe } from 'node:test';
 
-describe('Home Page Tests', () => {
-    test('Home Page Redirects to Login Page', async ({ page }, testInfo ) => {
+test.describe('Home Page Tests', () => {
+    test.beforeEach(async ({ page }) => {
         await page.goto('/');
+    });
+
+    test('Home Page Redirects to Login Page', async ({ page }) => {
         await expect(page).toHaveURL('/login');
+    });
+
+    test.afterEach(async ({ page }, testInfo) => {
+        console.log(`Test finished: ${testInfo.title}`);
+        // Optionally, you can take a screenshot after each test
+        // await page.screenshot({ path: `screenshots/${testInfo.title}.png` });
     });
 });
