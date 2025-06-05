13c52546d6e94c3ca3ca33abeb1e04a4aeafacfe
Selvakumar <vselva@gmail.com>
2025-06-05 17:21:37 +0530

    Test code added for login page fields (title, input field, button, links).

--- client/tests/home.spec.ts
+++ client/tests/home.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { describe } from 'node:test';
 
-
 describe('Home Page Tests', () => {
     test('Home Page Redirects to Login Page', async ({ page }, testInfo ) => {
         await page.goto('/');
--- client/tests/login.spec.ts
+++ client/tests/login.spec.ts
@@ -6,5 +6,23 @@ describe('Login Page Tests', () => {
         await page.goto('/login');
         await expect(page).toHaveTitle(/E-Commerce App/i);
         await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+        // form fields by textbox name
+        await expect(page.getByRole('textbox', { name: /email/i })).toBeVisible();
+        await expect(page.getByRole('textbox', { name: /password/i })).toBeVisible();
+
+        // form fields by placeholder name
+        await expect(page.getByPlaceholder('Enter your email')).toBeVisible();
+        await expect(page.getByPlaceholder('Enter your password')).toBeVisible();
+
+        // form buttons
+        await expect(page.getByRole('button', { name: /login/i })).toBeVisible();
+
+        // register link
+        await expect(page.getByRole('link', { name: 'Register', exact: true })).toBeVisible();
+        await expect(page.getByRole('link', { name: "Don't have an account? Register", exact: true })).toBeVisible();
+
+        // login link 
+        await expect(page.getByRole('link', { name: /Login/i })).toHaveAttribute('href', '/login');
     });
 });
