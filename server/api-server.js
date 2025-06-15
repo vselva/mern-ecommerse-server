55e1e83a3bcbbb8be0b6f0c4d6506cac9910061c
Selvakumar <vselva@gmail.com>
2025-06-15 20:24:29 +0530

    added helmet module, csfr modules,

--- server/api-server.js
+++ server/api-server.js
@@ -10,12 +10,29 @@ const NODE_ENV = process.env.NODE_ENV;
 const express = require('express');
 const app = express();
 
+// helmet 
+const helmet = require('helmet');
+app.use(helmet());
+
+// disable 'x-powered-by' header for security - this header can reveal information about the server
+if (NODE_ENV === 'production') {
+    app.use(helmet.hidePoweredBy());
+}
+
 // swagger
 const { swaggerUi, swaggerSpec } = require("./swagger");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // middleware to parase json requests 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' })); // limit the size of JSON payloads to 10kb
+
+// middleware to parse url-encoded requests
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); // limit the size of URL-encoded payloads to 10kb
+
+// middlware for csurf 
+// const csurf = require('csurf');
+// app.use(csurf());
+
 
 // cors
 const cors = require('cors');
